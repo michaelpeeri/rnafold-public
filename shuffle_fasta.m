45368af259cfdffe201e9d535dd21affc64085a4
Michael Peeri <mich1@post.tau.ac.il>
2016-06-06 22:12:15 +0300

    Renamed function to match name of m-file

--- shuffle_fasta.m
+++ shuffle_fasta.m
@@ -1,6 +1,6 @@
 % Read a (possibly large) fasta file; shuffly each sequence using shuffleCDS(); write the output to another fasta file.
 % TODO: geneticCode should be specified for each sequence, since different sequences from the same organism are translated using different codes.
-function []=shuffleFasta(filename,geneticCode)
+function []=shuffle_fasta(filename,geneticCode)
   % Read sequences from fasta file; write shuffled sequences to output file.
   biofile = BioIndexedFile('FASTA', filename);
   numSequences = biofile.NumEntries;
