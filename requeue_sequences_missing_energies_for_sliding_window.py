dde1c2305c5bb71241a9308bdb3ce3b137520e5c
Michael Peeri <mich1@post.tau.ac.il>
2016-06-28 01:42:19 +0300

    Updating with recent updates...

--- requeue_sequences_missing_energies_for_sliding_window.py
+++ requeue_sequences_missing_energies_for_sliding_window.py
@@ -1,69 +1,104 @@
 # Enqueue failed entries (i.e. entries missing the calculation results) for repeated processing.
 # Scan all CDS entries from a given taxid; Find entries missing a computation result; Re-insert them into the queue for repeated processing
-# Input - taxid
+# Input - taxid1,taxid2,taxid3
 #       - computationTag
+#       - randomFraction
 # Example:
-# python2 requeue_sequences_missing_energies_for_sliding_window.py 3055 rna-fold-window-40-0
+# python2 requeue_sequences_missing_energies_for_sliding_window.py 3055,556484 rna-fold-window-40-0 10
+# 
+# TODO: This script will requeue sequences that have already in the queue but haven't been completed yet.
 # TODO: Add support for step-size >1
 import sys
 import codecs
+from random import randint
 import redis
 import config
+import mysql_rnafold as db
+from sqlalchemy import sql
+from sqlalchemy.sql.expression import func
 
+# command-line arguments
+species = map(int, sys.argv[1].split(","))
 computationTag = sys.argv[2]
 if( computationTag.find(':') != -1 ): raise Exception("computation tag cannot contain ':' (should be compatible with redis key names)")
 # e.g. rna-fold-0
+randomFraction = int(sys.argv[3])
 
 # Configuration
 queueKey = "queue:tag:awaiting-%s:members" % computationTag
-calculationResultKey = "CDS:taxid:%%d:protid:%%s:computed:%s:value" % computationTag
-print(calculationResultKey)
+nativeCdsSeqIdKey = "CDS:taxid:%d:protid:%s:seq-id"
+seqLengthKey = "CDS:taxid:%d:protid:%s:length-nt"
 windowWidth = 40
+seriesSourceNumber = db.Sources.RNAfoldEnergy_SlidingWindow40
 # TODO: Add support for step-size >1
 
+# Establish DB connections
 r = redis.StrictRedis(host=config.host, port=config.port, db=config.db)
-
-taxIdForProcessing = int(sys.argv[1])
-print("Procesing %d sequences for tax-id %d (%s)..."
-    % (r.scard("species:taxid:%d:CDS" % taxIdForProcessing),
-    taxIdForProcessing,
-    r.get("species:taxid:%d:name" % taxIdForProcessing)))
+session = db.Session()
 
 skipped = 0
 selected = 0
+alreadyCompleted = 0
 
-# Iterate over all CDS entries for this species
-for protId in r.sscan_iter("species:taxid:%d:CDS" % taxIdForProcessing):
-    protId = codecs.decode(protId)
-    # Filtering
-    # Skip sequences with partial CDS annotations
-    if(r.exists("CDS:taxid:%d:protid:%s:partial" % (taxIdForProcessing, protId))):
-        skipped += 1
-        continue
+for taxIdForProcessing in species:
+    print("Procesing %d sequences for tax-id %d (%s)..."
+          % (r.scard("species:taxid:%d:CDS" % taxIdForProcessing),
+             taxIdForProcessing,
+             r.get("species:taxid:%d:name" % taxIdForProcessing)))
 
-    if(not r.exists("CDS:taxid:%d:protid:%s:seq" % (taxIdForProcessing, protId))):
-        print("Warning: couldn't find sequence for protid '%s'" % protId )
-        skipped += 1
-        continue
+    # Iterate over all CDS entries for this species
+    for protId in r.sscan_iter("species:taxid:%d:CDS" % taxIdForProcessing):
+        protId = codecs.decode(protId)
+        # Filtering
 
-    seqLength = r.strlen("CDS:taxid:%d:protid:%s:seq" % (taxIdForProcessing, protId))
-    # Skip sequences with length <40nt (window width)
-    if(seqLength < windowWidth ):
-        skipped += 1
-        continue
+        # Only process 1/N of the sequences, selected randomly (N=randomFraction)
+        # (if randomFraction==1, all sequences will be processed)
+        if( randint(1, randomFraction) != 1 ):
+            skipped += 1
+            continue
 
-    numWindows = seqLength - windowWidth + 1
+        # Skip sequences with partial CDS annotations
+        if(r.exists("CDS:taxid:%d:protid:%s:partial" % (taxIdForProcessing, protId))):
+            skipped += 1
+            continue
 
-    # Do the computed folding energies exist (and contain a value for each window?)
-    if( r.exists( calculationResultKey % (taxIdForProcessing, protId)) and
-        r.llen( calculationResultKey % (taxIdForProcessing, protId)) == numWindows ):
-        skipped += 1
-        continue
+        if( not r.exists(nativeCdsSeqIdKey % (taxIdForProcessing, protId)) ):
+            skipped +=1
+            continue
 
-    # Enqueue this protein for processing
-    r.rpush(queueKey, "%s:%s" % (taxIdForProcessing, protId))
-    selected += 1
+        seqLength = r.get(seqLengthKey % (taxIdForProcessing, protId))
+        if( not seqLength is None ):
+            # Skip sequences with length <40nt (window width)
+            seqLength = int(seqLength)
+            if(seqLength < windowWidth ):
+                skipped += 1
+                continue
+        else:
+            print("Warning: Could not find CDS length entry for taxid=%d, protid=%s" % (taxIdForProcessing, protId) )
+            skipped += 1
+            continue
+
+        requiredNumWindows = seqLength - windowWidth + 1
+
+        cdsSeqId = int(r.get(nativeCdsSeqIdKey % (taxIdForProcessing, protId)))
+
+        # Count the number of window results stored for this series (in the current sequence)
+        windowCount = db.connection.execute( sql.select(( sql.func.count('*'),)).select_from(db.sequence_series).where(
+                sql.and_(
+                    db.sequence_series.c.sequence_id==cdsSeqId,
+                    db.sequence_series.c.source==seriesSourceNumber,
+                    db.sequence_series.c.ext_index==0
+                    )) ).scalar()
+            # 
+        if( windowCount < requiredNumWindows ):
+            # enqueue this sequence for processing
+            # Note: all windows will be computed (even if partial results exist)
+            #print("%d:%s:%d" % (taxIdForProcessing, protId, currentSeqId))
+            r.rpush(queueKey, "%d:%s:%d" % (taxIdForProcessing, protId, cdsSeqId))
+            selected += 1
+        else:
+            alreadyCompleted += 1
 
 
-print("%d selected, %d skipped (%d total)" % (selected, skipped, selected+skipped))
+print("%d selected, %d skipped, %d already completed (%d total)" % (selected, skipped, alreadyCompleted, selected+skipped))
 print("queue contains %d items" % r.llen(queueKey))
