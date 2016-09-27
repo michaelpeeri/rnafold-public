6fd79e10bc9f2ee23c7b7867088867a31a93ac8c
Michael Peeri <mich1@post.tau.ac.il>
2016-09-27 21:44:28 +0300

    Adding pushover keys

--- config.py
+++ config.py
@@ -1,9 +1,17 @@
 # Site configuration file (for server details, etc.)
 
-host = "127.0.0.1"
+# redis
+host = "power5"
 port = 6379
 db = 0
+password = "rnafold"
 
+# mysql
+mysql_host_connection = 'mysql://rnafold:--password--@ec2-54-234-142-88.compute-1.amazonaws.com/rnafold'
 
-mysql_host_connection = 'mysql://rnafold:rnafold@localhost/rnafold'
+# Pushover
+# Application token
+computation_monitor_app = '--pushover-key--'
+# Group key
+computation_monitor_group = '--pushover-key--'
 
