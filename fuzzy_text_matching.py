318b8d846d61992604fbc7823cd51cb6bd2a3419
Michael Peeri <mich1@post.tau.ac.il>
2017-09-28 12:11:11 +0300

    Added mapping between alignment (s5) and tree (s6) identifiers

---
+++ fuzzy_text_matching.py
@@ -0,0 +1,65 @@
+import swalign # https://github.com/mbreese/swalign
+# conda install --channel https://conda.anaconda.org/bioconda swalign
+
+
+"""
+Scoring matrix for Needleman-Wunsch edit distance, for fuzzy matching taxon names
+"""
+class FuzzySpeciesNameScoringMatrix(object):
+    def __init__(self):
+        self._wildcard =  0
+        self._match    =  5
+        self._mismatch = -5
+        
+    def score( self, one, two, wildcard=None):
+        #if wildcard and (one in wildcard and two in wildcard):
+        #    return self._wildcard
+
+        if one.lower()==two.lower() and one.isalnum() and two.isalnum():  # Allow matching of characters, prevent treating whitespace as a match
+            #print(" '%s' '%s'" % (one, two))
+            return self._match
+
+        elif (not one.isalnum()) and (not two.isalnum()):  # Whitespace matches don't count
+            return 0
+        
+        elif one.lower()!=two.lower() and (one.isdigit() or two.isdigit()):   # Give a severe penalty for mismatching digits
+            return 10*self._mismatch
+        
+        else:
+            assert(one != two)
+            return self._mismatch
+    
+
+sw = swalign.LocalAlignment(
+        FuzzySpeciesNameScoringMatrix(),
+        -9, -1,
+    verbose=False, globalalign=False, full_query=True)
+
+def test():
+    #NCBI    = "Candidatus Curtissbacteria bacterium GW20 11 GWA1 40 16"
+    NCBI2    = "Curtissbacteria bacterium GW20 11 GWA1 40 16"
+    Nmicros6 = "Curtissbacteria GWA1 OP11 40 13 partial"
+    Nmicros7 = "Curtissbacteria GWA1 OP11 40 16 partial"
+    Nmicros8 = "Curtissbacteria GWA1 OP11 40 16"
+    Nmicros82 = "Curtissbacteria GWA1 OP11 40 13"
+
+    #sw.align( Nmicros6, NCBI2 ).dump()
+    #sw.align( NCBI2, Nmicros6 ).dump()
+    #sw.align( Nmicros6, Nmicros6 ).dump()
+    #sw.align( NCBI2, Nmicros8 ).dump()
+    sw.align( Nmicros8, NCBI2 ).dump()
+    #sw.align( Nmicros8, Nmicros8 ).dump()
+
+    #sw.align( NCBI2, Nmicros82 ).dump()
+    sw.align( Nmicros82, NCBI2 ).dump()
+
+    sw.align("Woesebacteria GWD2 OP11 40 19", "Candidatus Woesebacteria bacterium GW2011 GWD2 40 19").dump()
+
+    return 0
+
+
+def standalone():
+    import sys
+    sys.exit(test())
+if __name__=="__main__":
+    standalone()
