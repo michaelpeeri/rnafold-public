68ff2b9c173dcb82bbb5b9813b5911d01bca018c
Michael Peeri <mich1@post.tau.ac.il>
2019-01-21 19:53:39 +0200

    Open existing index db's (avoid recreating them)

--- cellfold/gff.py
+++ cellfold/gff.py
@@ -1,13 +1,18 @@
 # Access GFF data sources
 import gffutils  # See: http://daler.github.io/gffutils/contents.html
+import os
 
 # Get a GFF FeatureDB object for a given .gff file; Implement policies for this
 def createGffDb( filename, variant ):
 
-    db = gffutils.create_db(filename, dbfn="%s.db" % filename, force=True, keep_order=True,
-                            sort_attribute_values=True,
-                            merge_strategy='merge',
-                            id_spec=['ID', 'Name'])
+    if os.path.exists( filename ):
+        db = gffutils.FeatureDB("{}.db".format(filename), keep_order=True )
+        
+    else:
+        db = gffutils.create_db(filename, dbfn="{}.db".format(filename), force=True, keep_order=True,
+                                sort_attribute_values=True,
+                                merge_strategy='merge',
+                                id_spec=['ID', 'Name'])
     return db
 
 
