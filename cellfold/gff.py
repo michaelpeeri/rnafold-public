6e6a3dc72bd7345e5fb3837dc4c7b5d76c592aa1
Michael Peeri <mich1@post.tau.ac.il>
2019-01-30 05:44:52 +0200

    Fix: can now really initialize new dbs...

--- cellfold/gff.py
+++ cellfold/gff.py
@@ -5,11 +5,13 @@ import os
 # Get a GFF FeatureDB object for a given .gff file; Implement policies for this
 def createGffDb( filename, variant ):
 
-    if os.path.exists( filename ):
-        db = gffutils.FeatureDB("{}.db".format(filename), keep_order=True )
+    dbFilename = "{}.db".format(filename)
+    
+    if os.path.exists( dbFilename ):
+        db = gffutils.FeatureDB( dbFilename, keep_order=True )
         
     else:
-        db = gffutils.create_db(filename, dbfn="{}.db".format(filename), force=True, keep_order=True,
+        db = gffutils.create_db(filename, dbfn=dbFilename, force=True, keep_order=True,
                                 sort_attribute_values=True,
                                 merge_strategy='merge',
                                 id_spec=['ID', 'Name'])
